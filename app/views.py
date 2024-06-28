5dce5488af6531a68193c2ac393cb823752b7710
chandrikayadav <yadavchandrika789@gmail.com>
2024-06-28 12:47:25 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def nobitha(request):
+    return render(request,'nobitha.html')
+def sujuka(request):
+    return render(request,'sujuka.html')
